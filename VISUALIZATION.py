0efd05a6c9de4713ac24ea5d08ddaf83c60d913d
GRACE BHOKE <bhokegraciee@gmail.com>
2025-02-03 22:17:02 +0300

    add files

---
+++ VISUALIZATION.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+df=pd.read_csv(r'C:\Users\User\Desktop\DATA SCIENCE FINALYYYY\PYTHON\train_data_cross-sell.csv', encoding= "latin1")
+#(from sklearn.tree import DecisionTreeClassifier)
+df.head()
+import seaborn as sns
+import matplotlib as mpl
+from datetime import datetime 
